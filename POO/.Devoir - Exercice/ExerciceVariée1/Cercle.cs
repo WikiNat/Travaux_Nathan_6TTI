908efac130bf604296ea49fe893b463a91e8a3b3
WikiNat <nathan.marcq@icloud.com>
2025-12-21 15:39:52 +0100

    gg
    g

---
+++ POO/.Devoir - Exercice/ExerciceVariée1/Cercle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceVariée1
+{
+
+    public class Cercle
+    {
+        public double Rayon { get; set; }
+
+        public Cercle(double rayon)
+        {
+            Rayon = rayon;
+        }
+
+
+        public double CalculerAire()
+        {
+            return Math.PI * Rayon * Rayon;
+        }
+        public double CalculerPerimetre()
+        {
+            return 2 * Math.PI * Rayon;
+        }
+        public double CalculerMoitierPer()
+        {
+            return Rayon/2 *Math.PI *2 ;   
+        }
+        public double CaclulerMoitierAir() 
+        {
+            return Math.PI * Rayon/2 * Rayon/2;
+        }
+
+    }
+}
---
+++ POO/.Devoir - Exercice/ExerciceVariée1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExerciceVariée1;
+using Microsoft.Win32.SafeHandles;
+using System.Net.WebSockets;
+using System.Security.AccessControl;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string saisie ="";
+        do
+        {
+            Console.WriteLine("Quelle est le rayon de votre cercle ?");
+            double rayon = double.Parse(Console.ReadLine());
+            Cercle cercle = new Cercle(rayon);
+            Console.WriteLine("Ton cercle à un rayon de " + rayon + ", donc il a une aire de : " + cercle.CalculerAire() + " et un Périmètre de " + cercle.CalculerPerimetre());
+            Console.WriteLine("Avec un cercle de rayon diminué de moitié : \n-------------------------------------------");
+            Console.WriteLine("Ton cercle à un rayon de " + rayon / 2 + ", donc il a une aire de: " + cercle.CaclulerMoitierAir() + " et un Périmètre de " + cercle.CalculerMoitierPer());
+            Console.WriteLine("Vous avez un autre cercle? (Tapez sur espace) ");
+            saisie = Console.ReadLine();
+        } while (saisie == " ");
+    }
+}
